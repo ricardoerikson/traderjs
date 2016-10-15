17d917cdc7d71399973f5b2e456017f74231eeaf
Ricardo Erikson <ricardoerikson@gmail.com>
2016-10-15 07:28:57 -0400

    add JsonWriter to write JSON content to a file

---
+++ src/writer/json-writer.js
@@ -0,0 +1,20 @@
+import Writer from './writer';
+
+/**
+ * @classdesc Specific writer for JSON content.
+ * @class
+ * @augments Writer
+ */
+export default class JsonWriter extends Writer {
+
+    /**
+     * @inheritdoc
+     * @override
+     */
+    write(content, cb) {
+        this._fsObject.writeFile(this.filename, JSON.stringify(content), (err) => {
+            // if (err) throw err;
+            cb();
+        });
+    }
+}
